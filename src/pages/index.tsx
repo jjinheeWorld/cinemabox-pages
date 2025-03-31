0862253c1e46e34e2d134edbb3b60f438dbfe6c3
jjinheeWorld <wlsgml450@naver.com>
2025-04-01 03:36:10 +0900

    feat: SearchBar 기능 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
+import { ReactNode } from "react";
+import SearchableLayout from "./components/searchable-layout";
+
+export default function Home() {
   return <h1>CINEMABOX</h1>;
 }
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
