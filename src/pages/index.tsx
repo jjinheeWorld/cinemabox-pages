c6935d53dc53c121399ba8f17b9e48ca5a998bd3
jjinheeWorld <wlsgml450@naver.com>
2025-04-06 17:33:27 +0900

    style: 인덱스 페이지 UI 구현하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
 import { ReactNode } from "react";
 import SearchableLayout from "./components/searchable-layout";
+import movies from "../mock/dummy.json";
+import MovieItem from "./components/movie-item";
+import style from "./index.module.css";
 
 export default function Home() {
-  return <h1>CINEMABOX</h1>;
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 가장 추천하는 영화</h3>
+        <div className={style.recommend}>
+          {movies.slice(0, 3).map((movie) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </div>
+      </section>
+      <section>
+        <h3>등록된 모든 영화</h3>
+        <div className={style.all}>
+          {movies.map((movie) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
 
 Home.getLayout = (page: ReactNode) => {
