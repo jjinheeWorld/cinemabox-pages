79f3918cc7011cee48854411a762dc8379cd7f2c
jjinheeWorld <wlsgml450@naver.com>
2025-04-06 20:55:32 +0900

    refactor: css 수정 및 이미지 접근성 개선

--- src/pages/components/movie-item.tsx
+++ src/pages/components/movie-item.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 import type { MovieData } from "@/types";
 import style from "./movie-item.module.css";
 
-export default function MovieItem({ id, posterImgUrl }: MovieData) {
+export default function MovieItem({ id, title, posterImgUrl }: MovieData) {
   return (
     <Link href={`/movie/${id}`} className={style.container}>
-      <img src={posterImgUrl} />
+      <img src={posterImgUrl} alt={`${title} poster image`} />
     </Link>
   );
 }
