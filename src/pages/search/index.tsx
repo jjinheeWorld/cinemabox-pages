1c2426b40eb274f3553300aae1e76c0ba4898f60
jjinheeWorld <wlsgml450@naver.com>
2025-04-06 20:10:46 +0900

    style: 검색 페이지 UI 구현하기

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,12 +1,31 @@
 import { useRouter } from "next/router";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import SearchableLayout from "../components/searchable-layout";
+import movies from "@/mock/dummy.json";
+import MovieItem from "../components/movie-item";
+import style from "./index.module.css";
+import { MovieData } from "@/types";
 
 export default function Page() {
+  const [results, setResults] = useState<MovieData[]>([]);
   const router = useRouter();
-  const { q } = router.query;
+  const q = router.query.q as string;
+  console.log(q);
 
-  return <h1>검색 결과 :{q}</h1>;
+  useEffect(() => {
+    const data = movies.filter((movie) => movie.title.includes(q));
+    setResults(data);
+  }, [q]);
+
+  return (
+    <div className={style.container}>
+      <div className={style.search}>
+        {results.map((result) => (
+          <MovieItem key={result.id} {...result} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
