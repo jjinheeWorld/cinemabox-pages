c1a3106f214697f65352ccec7c12eee50ca6dfe1
jjinheeWorld <wlsgml450@naver.com>
2025-04-11 01:47:38 +0900

    feat: 검색 페이지 데이터 페칭 구현하기

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,27 +1,29 @@
-import { useRouter } from "next/router";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode } from "react";
 import SearchableLayout from "../components/searchable-layout";
-import movies from "@/mock/dummy.json";
 import MovieItem from "../components/movie-item";
 import style from "./index.module.css";
-import { MovieData } from "@/types";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchMovies from "@/lib/fetch-movies";
 
-export default function Page() {
-  const [results, setResults] = useState<MovieData[]>([]);
-  const router = useRouter();
-  const q = router.query.q as string;
-  console.log(q);
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const movies = await fetchMovies(q as string);
 
-  useEffect(() => {
-    const data = movies.filter((movie) => movie.title.includes(q));
-    setResults(data);
-  }, [q]);
+  return {
+    props: { movies },
+  };
+};
 
+export default function Page({
+  movies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <div className={style.search}>
-        {results.map((result) => (
-          <MovieItem key={result.id} {...result} />
+        {movies.map((movie) => (
+          <MovieItem key={movie.id} {...movie} />
         ))}
       </div>
     </div>
