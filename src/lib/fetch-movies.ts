f8122b03c21edfa720c25923d37bc63422dd0b04
jjinheeWorld <wlsgml450@naver.com>
2025-04-11 01:33:51 +0900

    feat: 인덱스 페이지 데이터 페칭 구현하기

---
+++ src/lib/fetch-movies.ts
@@ -0,0 +1,17 @@
+import { MovieData } from "@/types";
+
+export default async function fetchMovies(): Promise<MovieData[]> {
+  const url = `http://localhost:12345/movie`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    return response.json();
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,35 @@
 import { ReactNode } from "react";
 import SearchableLayout from "./components/searchable-layout";
-import movies from "../mock/dummy.json";
 import MovieItem from "./components/movie-item";
 import style from "./index.module.css";
+import fetchMovies from "@/lib/fetch-movies";
+import { InferGetServerSidePropsType } from "next";
+import fetchRandomMovies from "@/lib/fetch-random-movies";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const [allMovies, recoMovies] = await Promise.all([
+    fetchMovies(),
+    fetchRandomMovies(),
+  ]);
+
+  return {
+    props: {
+      allMovies,
+      recoMovies,
+    },
+  };
+};
+
+export default function Home({
+  allMovies,
+  recoMovies,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
         <div className={style.recommend}>
-          {movies.slice(0, 3).map((movie) => (
+          {recoMovies.map((movie) => (
             <MovieItem key={movie.id} {...movie} />
           ))}
         </div>
@@ -18,7 +37,7 @@ export default function Home() {
       <section>
         <h3>등록된 모든 영화</h3>
         <div className={style.all}>
-          {movies.map((movie) => (
+          {allMovies.map((movie) => (
             <MovieItem key={movie.id} {...movie} />
           ))}
         </div>
