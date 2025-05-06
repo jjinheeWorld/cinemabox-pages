4af1fa91cbef95260c8c1fbaba2f3a6e5371a543
jjinheeWorld <wlsgml450@naver.com>
2025-05-06 17:52:18 +0900

    feat: 파비콘 추가 및 페이지별 메타태그 설정으로 SEO 개선

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import style from "./index.module.css";
 import fetchMovies from "@/lib/fetch-movies";
 import { InferGetStaticPropsType } from "next";
 import fetchRandomMovies from "@/lib/fetch-random-movies";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
   const [allMovies, recoMovies] = await Promise.all([
@@ -26,24 +27,35 @@ export default function Home({
   recoMovies,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 가장 추천하는 영화</h3>
-        <div className={style.recommend}>
-          {recoMovies.map((movie) => (
-            <MovieItem key={movie.id} {...movie} />
-          ))}
-        </div>
-      </section>
-      <section>
-        <h3>등록된 모든 영화</h3>
-        <div className={style.all}>
-          {allMovies.map((movie) => (
-            <MovieItem key={movie.id} {...movie} />
-          ))}
-        </div>
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>씨네마박스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="씨네마박스" />
+        <meta
+          property="og:description"
+          content="씨네마박스에 등록된 영화를 만나보세요."
+        />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 가장 추천하는 영화</h3>
+          <div className={style.recommend}>
+            {recoMovies.map((movie) => (
+              <MovieItem key={movie.id} {...movie} />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h3>등록된 모든 영화</h3>
+          <div className={style.all}>
+            {allMovies.map((movie) => (
+              <MovieItem key={movie.id} {...movie} />
+            ))}
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -5,6 +5,7 @@ import style from "./index.module.css";
 import fetchMovies from "@/lib/fetch-movies";
 import { useRouter } from "next/router";
 import { MovieData } from "@/types";
+import Head from "next/head";
 
 export default function Page() {
   const [movies, setMovies] = useState<MovieData[]>([]);
@@ -23,13 +24,24 @@ export default function Page() {
   }, [q]);
 
   return (
-    <div className={style.container}>
-      <div className={style.search}>
-        {movies.map((movie) => (
-          <MovieItem key={movie.id} {...movie} />
-        ))}
+    <>
+      <Head>
+        <title>씨네마박스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="씨네마박스" />
+        <meta
+          property="og:description"
+          content="씨네마박스에 등록된 영화를 만나보세요."
+        />
+      </Head>
+      <div className={style.container}>
+        <div className={style.search}>
+          {movies.map((movie) => (
+            <MovieItem key={movie.id} {...movie} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
